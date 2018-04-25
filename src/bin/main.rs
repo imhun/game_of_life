ca951f91828e49bc365d96a7b8f915debdecf0d4
huyang <huwsun@gmail.com>
2018-04-25 23:28:41 +0800

    update

---
+++ src/bin/main.rs
@@ -0,0 +1,7 @@
+//extern crate wasm_game_of_life;
+//use wasm_game_of_life::Universe;
+fn main() {
+    //let un=Universe::new();
+    //let ptr=un.cells();
+  //  println!("{}", Universe::new().render());
+}
